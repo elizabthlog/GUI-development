4ee07cc2cd4c5dcd697769ae8f03efafd3456d30
elizabthlog <nobld.elizadenise@gmail.com>
2026-05-09 21:46:44 +0800

    changed color

--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import random
 root = tk.Tk()
 root.geometry("500x500")
-root.configure(background="#b3c330")
+root.configure(background="#a4c330")
 root.mainloop()
 root.title("Eliza's Number Guessing Game")
 
